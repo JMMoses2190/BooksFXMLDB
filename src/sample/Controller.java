45bf2639f34335c143cc4ad8d84a9b1f8b0fe7b2
Jordan Moses <jmmoses2190@eagle.fgcu.edu>
2018-12-13 14:40:49 -0500

    Database Draft

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,65 +1,90 @@
 package sample;
 
-import java.awt.Button;
-import java.awt.event.ActionEvent;
+
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.text.TextFlow;
 
 public class Controller {
 
   private static int recordNumber = 1;
 
-  final String DATABASE_URL = "jdbc:derby:C:\\Users\\Jorda\\IdeaProjects\\BooksDBFX\\lib\\books";
+  final String DATABASE_URL = "jdbc:derby:lib\\books";
+  final String SELECT_QUERY =
+      "SELECT authorID, firstName, lastName FROM authors";
+
+
+    @FXML
+    private TextFlow bookID;
+
+    @FXML
+    private TextField enterFirstName;
+
+    @FXML
+    private Label lblFiName;
+
+    @FXML
+    private TextField enterLastName;
+
+    @FXML
+    private Label lblLaName;
+
+    //Code currently displays as entry into the database
+    @FXML
+    void handleString(ActionEvent event) {
+      String INSERT_QUERY = String.format("INSERT INTO AUTHORS(firstName, lastName)"
+          + "VALUES ('%s', '%s')", enterFirstName.getText(), enterLastName.getText());
+      connectAndExecute(INSERT_QUERY);
+
+      System.out.println("Entry Added!");
+
+    }
+  @FXML
+  private Button newEntry;
+
 
   @FXML
-
-  private TextField enterFirstName;
-
-  @FXML
-  private TextField enterLastName;
-
-
-  @FXML
-  private void handleString(ActionEvent event) {
-    String INSERT_QUERY = String.format("INSERT INTO AUTHORS(firstName, lastName)"
-        + "VALUES ('%s', '%s')", enterFirstName.getText(), enterLastName.getText());
-    connectAndExecute(INSERT_QUERY);
-
-    System.out.println("Entry Added!");
+  void newString(ActionEvent event) {
 
   }
 
+
+  //connects to database
   private void connectAndExecute(String query) {
-    Properties prop = new Properties();
-    try (FileInputStream in = new FileInputStream("dir/db.properties")) {
-      prop.load(in);
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-    } catch (IOException e) {
-      e.printStackTrace();
+    try (
+        Connection connection = DriverManager.getConnection(
+            DATABASE_URL, "deitel", "deitel");
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(SELECT_QUERY))
+    {
+
+      // display query results
+      resultSet.next();
+
+      System.out.println(resultSet.getString(2));
+
+    } // AutoCloseable objects' close methods are called now
+    catch (SQLException sqlException)
+    {
+      sqlException.printStackTrace();
     }
 
-    String uname = prop.getProperty("jdbc.username");
-    String pword = prop.getProperty("jdbc.password");
-
-    try (Connection connection = DriverManager.getConnection(DATABASE_URL, uname, pword);
-        Statement statement = connection.createStatement()) {
-      try {
-        statement.execute(query);
-      } catch (Exception ex) {
-        ex.printStackTrace();
-      }
-    } catch (SQLException sqlEx) {
-      sqlEx.printStackTrace();
+    catch ( Exception ex){
+      System.out.println("Some other Exception");
     }
   }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,6 +8,7 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+  //Starts the fxml file
   @Override
   public void start(Stage primaryStage) throws Exception {
     Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
